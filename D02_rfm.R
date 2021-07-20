4e903e6d2a237df4c8b54d31c891b67c3b7ffcc5
lorenzlorg <lorenzlorg@gmail.com>
2021-07-21 01:21:33 +0200

    quasi versione finale

--- D02_rfm.R
+++ D02_rfm.R
@@ -1,4 +1,8 @@
 ### RFM MODEL: provides a deterministic description of the value of each customer in term of purchase behaviour
+
+# ! Modificare un po la suddivisione delle classi
+# ! Vedi aggiunte grafici
+
 # Le metriche utilizzate sono:
 # Recency: How recently a customer has made a purchase
 # Frequency: How often a customer makes a purchase
@@ -15,56 +19,86 @@
 # le rilevazioni vanno dal 01/05/2018 al 30/04/2019
 # si decide di utilizzare come reference date il 28/02/2018
 
-rfm_threshold<-df_7_tic_clean_final  %>%
+rfm_data<-df_7_tic_clean_final  %>%
   filter(TIC_DATE > as.Date("28/02/2019", format = "%d/%m/%Y"))
+rfm_data
 
-rfm_threshold
 
+#### RECENCY VALUE: days passed from last purchase ####
 
-#### RECENCY: days passed from last purchase ####
-
-my_data_recency<-rfm_threshold %>%
+recency_data<-rfm_data %>%
                 filter(DIREZIONE==1) %>%
                 group_by(ID_CLI) %>%
                 summarise(LAST_PURCHASE_DATE=max(TIC_DATE))
 
-my_data_recency
+recency_data
 
 # costruisco la variabile RECENCY 
 
-my_data_recency$RECENCY<-difftime(as.Date("30/04/2019",
+recency_data$RECENCY_VALUE<-difftime(as.Date("30/04/2019",
                                       format="%d/%m/%Y"),
-                                  my_data_recency$LAST_PURCHASE_DATE,
+                                  recency_data$LAST_PURCHASE_DATE,
                               units = "days")
-
-my_data_recency$RECENCY
-
 # la recency è ottenuta come differenza tra l'ultimo giorno di acquisto per cliente e l'ultimo giorno di rilevazione
-# as esempio: il cliente con ID 5 ha acquistato l'ultima volta il 23 novembre 2018, ovvero 158 giorno entro l'ultima data di rilevazione.
+# as esempio: il cliente con ID 5 ha acquistato l'ultima volta il 23 novembre 2018, ovvero 158 giorni entro l'ultima data di rilevazione
 
 
+#### FREQUENCY VALUE: total number of purchase in the reference range ####
 
-# suddivido la recency in:
-# low: below the 25th percentile of the distribution
-# medium: from 25th to 75th percentile
-# high: above 75th percentile
+frequency_data <- rfm_data %>%
+  filter(DIREZIONE == 1) %>% 
+  group_by(ID_CLI)  %>% 
+  summarise(FREQUENCY_VALUE = n_distinct(ID_SCONTRINO)) %>%
+  arrange(desc(FREQUENCY_VALUE))
 
-
-## definisco le classi per la variabile recency e ottengo i percentili di classe
-summary(my_data_recency)
-
-
-my_data_recency<- within(my_data_recency,
-                      RECENCY_CLASS <- cut(as.numeric(my_data_recency$RECENCY),
-                                       breaks = quantile(my_data_recency$RECENCY,
-                                                         probs = c(0, .25, .75, 1)), 
-                                       include.lowest = T,
-                                       labels = c("low", "medium", "high")))         
-
-recency_var <- as.data.frame(table(my_data_recency$RECENCY_CLASS))
+frequency_data
 
 
-#PLOT DISTRIBUTION
+#### MONETARY VALUE: amount spent in the reference range ####
+
+monetary_data <- rfm_data %>%
+  filter(DIREZIONE == 1) %>% 
+  group_by(ID_CLI) %>% 
+  summarize(IMPORTO_LORDO = sum(IMPORTO_LORDO),
+            SCONTO = sum(SCONTO),
+            MONETARY_VALUE = IMPORTO_LORDO - SCONTO) %>%
+  ungroup() %>%
+  as.data.frame() %>%
+  arrange(desc(IMPORTO_LORDO))
+
+monetary_data
+
+
+#### Merge Recency, Frequency, Monetary ####
+
+rfm_data_clean <- merge(frequency_data,
+             monetary_data,  
+             by = "ID_CLI") 
+
+rfm_data_clean <- merge(rfm_data_clean,           
+             recency_data,  
+             by = "ID_CLI")
+
+rfm_data_clean <- rfm_data_clean[,c(1,2,5,7)]
+
+# analizzo le distribuzioni
+hist(as.numeric(rfm_data_clean$RECENCY_VALUE), main = "Distribution RECENCY")
+hist(as.numeric(rfm_data_clean$FREQUENCY_VALUE), main = "Distribution FREQUENCY")
+hist(as.numeric(rfm_data_clean$MONETARY_VALUE), main = "Distribution MONETARY")
+
+#### RECENCY CLASS ####
+
+rfm_data_clean<- within(rfm_data_clean,
+                        RECENCY_CLASS <- cut(as.numeric(rfm_data_clean$RECENCY_VALUE),
+                                             breaks = quantile(rfm_data_clean$RECENCY_VALUE,
+                                                               probs = c(0, .25, .75, 1)), 
+                                             include.lowest = T,
+                                             labels = c("low", "medium", "high")))         
+
+rfm_data_clean %>% 
+  group_by(RECENCY_CLASS) %>%
+  summarise(Count = n())
+
 ggplot(data = recency_var,
        aes(x = Var1, y = Freq,
            fill = Freq)) +                       
@@ -81,36 +115,20 @@ ggplot(data = recency_var,
 # N.B la classe recency high corrisponde alla classe con il maggior numero di giorni dall'ultimo acquisto
 
 
-#### FREQUENCY: total number of purchase in the reference range ####
 
-my_data_frequency <- rfm_threshold %>%
-  filter(DIREZIONE == 1) %>% 
-  group_by(ID_CLI)  %>% 
-  summarise(TOT_PURCHASE = n_distinct(ID_SCONTRINO)) %>%
-  arrange(desc(TOT_PURCHASE))
+#### FREQUENCY CLASS ####
 
-my_data_frequency
+rfm_data_clean <- within(rfm_data_clean,
+                         FREQUENCY_CLASS <- cut(rfm_data_clean$FREQUENCY_VALUE,
+                                                breaks = c(0, 2, 5, 101),             
+                                                include.lowest = T,
+                                                right = F,
+                                                labels = c("low", "medium", "high"))) 
+rfm_data_clean %>% 
+  group_by(FREQUENCY_CLASS) %>%
+  summarise(Count = n())
 
-# suddivo in:
-# low: below 2 total purchases
-# medium: from 2 to 5 purchases
-# high: above 5 (to 101) purchases
-
-# costruisco le classi 
-
-my_data_frequency <- within(my_data_frequency,
-                        FREQUENCY_CLASS <- cut(my_data_frequency$TOT_PURCHASE,
-                                          breaks = c(0, 2, 5, 101),             
-                                          include.lowest = T,
-                                          right = F,
-                                          labels = c("low", "medium", "high"))) 
-table(my_data_frequency$FREQUENCY_CLASS)
-
-
-frequency_var<- as.data.frame(table(my_data_frequency$FREQUENCY_CLASS))
-
-
-# PLOT DISTRIBUTION
+frequency_var <- as.data.frame(table(rfm_data_clean$FREQUENCY_CLASS))
 
 ggplot(data = frequency_var,
        aes(x = Var1, y = Freq,
@@ -127,40 +145,21 @@ ggplot(data = frequency_var,
 # la maggior parte degli acquisti (41817) appartiene  alla classe con minor frequenza di acquisto
 
 
-#### MONETARY VALUE: amount spent in the reference range ####
 
-my_data_monetary_value <- rfm_threshold %>%
-  filter(DIREZIONE == 1)%>% 
-  group_by(ID_CLI) %>% 
-  summarize(IMPORTO_LORDO = sum(IMPORTO_LORDO),
-            SCONTO = sum(SCONTO),
-            IMPORTO_NETTO = IMPORTO_LORDO - SCONTO) %>%
-  ungroup()                                 %>%
-  as.data.frame()                           %>%
-  arrange(desc(IMPORTO_LORDO))
+#### MONETARY CLASS ####
 
-my_data_monetary_value
+rfm_data_clean <- within(rfm_data_clean,
+                        MONETARY_CLASS <- cut(rfm_data_clean$MONETARY_VALUE,
+                                              breaks = quantile(rfm_data_clean$MONETARY_VALUE,
+                                                                probs = c(0, .25, .75, 1)),
+                                              include.lowest = T,
+                                              
+                                              labels = c("low", "medium", "high"))) 
+rfm_data_clean %>% 
+  group_by(MONETARY_CLASS) %>%
+  summarise(Count = n())
 
-# suddivido in:
-# low: below the 25th percentile of the distribution
-# medium: from 25th to 75th percentile
-# high: above 75th percentile
-
-
-# Costruisco le classi
-
-my_data_monetary_value <- within(my_data_monetary_value,
-                       MONETARY_CLASS <- cut(my_data_monetary_value$IMPORTO_NETTO,
-                                        breaks = quantile(my_data_monetary_value$IMPORTO_NETTO,
-                                                          probs = c(0, .25, .75, 1)),
-                                        include.lowest = T,
-                               
-                                                 labels = c("low", "medium", "high"))) 
-table(my_data_monetary_value$MONETARY_CLASS)
-
-monetary_var <- as.data.frame(table(my_data_monetary_value$MONETARY_CLASS))
-
-# PLOT DISTRIBUTION
+monetary_var <- as.data.frame(table(rfm_data_clean$MONETARY_CLASS))
 
 ggplot(data = monetary_var,
        aes(x = Var1, y = Freq,
@@ -175,48 +174,130 @@ ggplot(data = monetary_var,
   guides(fill = FALSE)+
   geom_text(aes(label=Freq), position=position_dodge(width=0.9), vjust=-0.25)
 
-
 # la classe più lucrosa, in termini monetari è la medium, ovvero 38275 degli acquisti sono abbastanza costosi
 
-# unisco i tre dataset:
 
-rfm <- merge(my_data_frequency,
-             my_data_monetary_value,  
-             by = "ID_CLI") 
 
-rfm <- merge(rfm,           
-             my_data_recency,  
-             by = "ID_CLI")
+#### RECENCY E FREQUENCY COMBINED ####
+# in modo tale che si vengano a creare delle nuove classi che descrivono i clienti
 
-summary(rfm)
-str(rfm)
-
-# combino le varie classi per la segmentazione della customer base
-
-rfm$recency_frequency <- NA
-# table(is.na(rfm$RECENCY_CLASS))
-# table(is.na(rfm$FREQUENCY_CLASS))
-# rfm<-na.omit(rfm)
-# table(is.na(rfm))
-for(i in c(1:nrow(rfm))){
-  if(rfm$RECENCY_CLASS[i] == "low" && rfm$FREQUENCY_CLASS[i] == "low") rfm$recency_frequency[i] <- "One-Timer"
-  if(rfm$RECENCY_CLASS[i] == "medium" && rfm$FREQUENCY_CLASS[i] == "low") rfm$recency_frequency[i] <- "One-Timer"
-  if(rfm$RECENCY_CLASS[i] == "high" && rfm$FREQUENCY_CLASS[i] == "low") rfm$recency_frequency[i] <- "Leaving"
-  if(rfm$RECENCY_CLASS[i] == "low" && rfm$FREQUENCY_CLASS[i] == "medium") rfm$recency_frequency[i] <- "Engaged"
-  if(rfm$RECENCY_CLASS[i] == "medium" && rfm$FREQUENCY_CLASS[i] == "medium") rfm$recency_frequency[i] <- "Engaged"
-  if(rfm$RECENCY_CLASS[i] == "high" && rfm$FREQUENCY_CLASS[i] == "medium") rfm$recency_frequency[i] <- "Leaving"
-  if(rfm$RECENCY_CLASS[i] == "low" && rfm$FREQUENCY_CLASS[i] == "high") rfm$recency_frequency[i] <- "Top"
-  if(rfm$RECENCY_CLASS[i] == "medium" && rfm$FREQUENCY_CLASS[i] == "high") rfm$recency_frequency[i] <- "Top"
-  if(rfm$RECENCY_CLASS[i] == "high" && rfm$FREQUENCY_CLASS[i] == "high") rfm$recency_frequency[i] <- "Leaving Top"
+rfm_data_clean$RECENCY_FREQUENCY <- NA
+for(i in c(1:nrow(rfm_data_clean))){
+  if(rfm_data_clean$RECENCY_CLASS[i] == "low" && rfm_data_clean$FREQUENCY_CLASS[i] == "low") rfm_data_clean$RECENCY_FREQUENCY[i] <- "One-Timer"
+  if(rfm_data_clean$RECENCY_CLASS[i] == "medium" && rfm_data_clean$FREQUENCY_CLASS[i] == "low") rfm_data_clean$RECENCY_FREQUENCY[i] <- "One-Timer"
+  if(rfm_data_clean$RECENCY_CLASS[i] == "high" && rfm_data_clean$FREQUENCY_CLASS[i] == "low") rfm_data_clean$RECENCY_FREQUENCY[i] <- "Leaving"
+  if(rfm_data_clean$RECENCY_CLASS[i] == "low" && rfm_data_clean$FREQUENCY_CLASS[i] == "medium") rfm_data_clean$RECENCY_FREQUENCY[i] <- "Engaged"
+  if(rfm_data_clean$RECENCY_CLASS[i] == "medium" && rfm_data_clean$FREQUENCY_CLASS[i] == "medium") rfm_data_clean$RECENCY_FREQUENCY[i] <- "Engaged"
+  if(rfm_data_clean$RECENCY_CLASS[i] == "high" && rfm_data_clean$FREQUENCY_CLASS[i] == "medium") rfm_data_clean$RECENCY_FREQUENCY[i] <- "Leaving"
+  if(rfm_data_clean$RECENCY_CLASS[i] == "low" && rfm_data_clean$FREQUENCY_CLASS[i] == "high") rfm_data_clean$RECENCY_FREQUENCY[i] <- "Top"
+  if(rfm_data_clean$RECENCY_CLASS[i] == "medium" && rfm_data_clean$FREQUENCY_CLASS[i] == "high") rfm_data_clean$RECENCY_FREQUENCY[i] <- "Top"
+  if(rfm_data_clean$RECENCY_CLASS[i] == "high" && rfm_data_clean$FREQUENCY_CLASS[i] == "high") rfm_data_clean$RECENCY_FREQUENCY[i] <- "Leaving Top"
 }
 
-table(rfm$recency_frequency)
+rfm_data_clean %>% 
+  group_by(RECENCY_FREQUENCY) %>%
+  summarise(Count = n())
+
+
+recency_frequency_var <- as.data.frame(table(rfm_data_clean$RECENCY_FREQUENCY))
+
+ggplot(data = recency_frequency_var,
+       aes(x = Var1, y = Freq,
+           fill = Freq)) +                       
+  geom_bar(stat = "identity", fill="turquoise3") +                  
+  scale_colour_brewer(palette = "Spectral") +
+  labs(x     = "Recency Frequency",
+       y     = "Total Amount") +                  
+  theme_classic() +                               
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  scale_x_discrete(labels = c("Engaged", "Leaving", "Leaving Top", "One-Timer", "Top")) + 
+  guides(fill = FALSE)+
+  geom_text(aes(label=Freq), position=position_dodge(width=0.9), vjust=-0.25)
 
 # One-Timer: clienti che hanno acquistato recentemente/abbastanza recentementente ma con scarsa frequenza
 # Leaving: clienti che non hanno acquistato recentemente e con scarsa/media frequenza
 # Engaged: clienti che hanno acquistato recentemente/abbastanza recentemente e con media frequenza
 # Top: cienti che hanno acquistato recentemnte/abbastanza recentemente e con alta frequenza
 # Leaving Top: cienti che non hanno acquistato di recente ma con alta frequenza
+
+# ! aggiungo qualche grafico
+
+#### RECENCY E FREQUENCY COMBINED with MONETARY ####
+
+rfm_data_clean$RECENCY_FREQUENCY_MONETARY <- NA
+
+for(i in c(1:nrow(rfm_data_clean))){
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Top" && rfm_data_clean$MONETARY_CLASS[i] == "low") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Silver"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Leaving Top" && rfm_data_clean$MONETARY_CLASS[i] == "low") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Bronze"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Engaged" && rfm_data_clean$MONETARY_CLASS[i] == "low") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Copper"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Leaving" && rfm_data_clean$MONETARY_CLASS[i] == "low") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Tin"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "One-Timer" && rfm_data_clean$MONETARY_CLASS[i] == "low") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Cheap"
+  
+  
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Top" && rfm_data_clean$MONETARY_CLASS[i] == "medium") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Gold"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Leaving Top" && rfm_data_clean$MONETARY_CLASS[i] == "medium") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Silver"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Engaged" && rfm_data_clean$MONETARY_CLASS[i] == "medium") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Bronze"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Leaving" && rfm_data_clean$MONETARY_CLASS[i] == "medium") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Copper"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "One-Timer" && rfm_data_clean$MONETARY_CLASS[i] == "medium") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Tin"
+  
+  
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Top" && rfm_data_clean$MONETARY_CLASS[i] == "high") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Diamond"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Leaving Top" && rfm_data_clean$MONETARY_CLASS[i] == "high") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Gold"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Engaged" && rfm_data_clean$MONETARY_CLASS[i] == "high") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Silver"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "Leaving" && rfm_data_clean$MONETARY_CLASS[i] == "high") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Bronze"
+  if(rfm_data_clean$RECENCY_FREQUENCY[i] == "One-Timer" && rfm_data_clean$MONETARY_CLASS[i] == "high") rfm_data_clean$RECENCY_FREQUENCY_MONETARY[i] <- "Copper"
+  
+}
+
+rfm_data_clean %>% 
+  group_by(RECENCY_FREQUENCY_MONETARY) %>%
+  summarise(Count = n())
+
+
+recency_frequency_monetary_var <- as.data.frame(table(rfm_data_clean$RECENCY_FREQUENCY_MONETARY))
+
+ggplot(data = recency_frequency_monetary_var,
+       aes(x = Var1, y = Freq,
+           fill = Freq)) +                       
+  geom_bar(stat = "identity", fill="turquoise3") +                  
+  scale_colour_brewer(palette = "Spectral") +
+  labs(x     = "Recency Frequency Monetary",
+       y     = "Total Amount") +                  
+  theme_classic() +                               
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  scale_x_discrete(labels = c("Bronze", "Cheap", "Copper", "Diamond", "Gold", "Silver", "Tin")) + 
+  guides(fill = FALSE)+
+  geom_text(aes(label=Freq), position=position_dodge(width=0.9), vjust=-0.25)
+
+# Bronze-Timer: 
+# Cheap: 
+# Copper: 
+# Diamond: 
+# Gold: 
+# Silver:
+# Tin:
+
+# ! aggiungo qualche grafico
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # costruisco un dataset con le informazioni ottenute
@@ -368,10 +449,10 @@ library(rfm)
 library(lubridate)
 library(magrittr)
 
-df_7_tic_clean_final$IMPORTO_NETTO<-(df_7_tic_clean_final$IMPORTO_LORDO)-(df_7_tic_clean_final$SCONTO)
+df_7_tic_clean_final$MONETARY_VALUE<-(df_7_tic_clean_final$IMPORTO_LORDO)-(df_7_tic_clean_final$SCONTO)
 analysis_date<-lubridate::as_date("2019/04/30")
 
-result<-rfm_table_order(df_7_tic_clean_final, ID_CLI, TIC_DATE, IMPORTO_NETTO, analysis_date)
+result<-rfm_table_order(df_7_tic_clean_final, ID_CLI, TIC_DATE, MONETARY_VALUE, analysis_date)
 
 rfm_results<-as.data.frame(result$rfm)
 
